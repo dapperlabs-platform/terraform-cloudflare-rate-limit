572e5e5c6ed8a9a63bc8713e164970c244a3bcc7
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-30 14:02:12 -0800

    update to ruleset resource

--- main.tf
+++ main.tf
@@ -6,48 +6,76 @@ data "cloudflare_zones" "zone" {
   }
 }
 
-resource "cloudflare_rate_limit" "limit" {
-  zone_id             = lookup(data.cloudflare_zones.zone.zones[0], "id")
-  threshold           = var.threshold
-  period              = var.period
-  disabled            = var.disabled
-  description         = var.description
-  bypass_url_patterns = var.bypass_url_patterns
+#resource "cloudflare_rate_limit" "limit" {
+#  zone_id             = lookup(data.cloudflare_zones.zone.zones[0], "id")
+#  threshold           = var.threshold
+#  period              = var.period
+#  disabled            = var.disabled
+#  description         = var.description
+#  bypass_url_patterns = var.bypass_url_patterns
+#
+#  match {
+#    request {
+#      url_pattern = var.request.url_pattern
+#      schemes     = var.request.schemes
+#      methods     = var.request.methods
+#    }
+#
+#    dynamic "response" {
+#      for_each = var.response == null ? [] : [1]
+#      content {
+#        statuses       = var.response.statuses
+#        origin_traffic = var.response.origin_traffic
+#        headers        = var.response.headers
+#      }
+#    }
+#  }
+#
+#  action {
+#    mode    = var.action.mode
+#    timeout = var.action.timeout
+#
+#    dynamic "response" {
+#      for_each = try(var.action.response, null) == null ? [] : [1]
+#      content {
+#        content_type = var.action.response.content_type
+#        body         = var.action.response.body
+#      }
+#    }
+#  }
+#
+#  dynamic "correlate" {
+#    for_each = var.correlate_by == null ? [] : [1]
+#    content {
+#      by = var.correlate_by
+#    }
+#  }
+#}
+#
+#
+resource "cloudflare_ruleset" "zone_level_ratelimit" {
+  zone_id     = lookup(data.cloudflare_zones.zone.zones[0], "id")
+  name        = "Rate limiting for my zone"
+  description = ""
+  kind        = "zone"
+  phase       = "http_ratelimit"
 
-  match {
-    request {
-      url_pattern = var.request.url_pattern
-      schemes     = var.request.schemes
-      methods     = var.request.methods
-    }
-
-    dynamic "response" {
-      for_each = var.response == null ? [] : [1]
-      content {
-        statuses       = var.response.statuses
-        origin_traffic = var.response.origin_traffic
-        headers        = var.response.headers
-      }
-    }
-  }
-
-  action {
-    mode    = var.action.mode
-    timeout = var.action.timeout
-
-    dynamic "response" {
-      for_each = try(var.action.response, null) == null ? [] : [1]
-      content {
-        content_type = var.action.response.content_type
-        body         = var.action.response.body
-      }
-    }
-  }
-
-  dynamic "correlate" {
-    for_each = var.correlate_by == null ? [] : [1]
+  dynamic "rules" {
+    for_each = var.ratelimit_rules
     content {
-      by = var.correlate_by
+      action      = rules.value.action
+      expression  = rules.value.expression
+      description = rules.value.description
+      enabled     = rules.value.enabled
+      dynamic "ratelimit" {
+        for_each = var.ratelimit_rules
+        content {
+          characteristics     = ratelimit.value.characteristics
+          period              = ratelimit.value.period
+          requests_per_period = ratelimit.value.requests_per_period
+          mitigation_timeout  = ratelimit.value.mitigation_timeout
+        }
+      }
     }
   }
 }
