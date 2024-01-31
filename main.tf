cca8c8020812f7547b105c917bbb6d0d8527bc1a
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-31 11:10:25 -0800

    finalise

--- main.tf
+++ main.tf
@@ -9,53 +9,6 @@ data "cloudflare_zones" "zones" {
   }
 }
 
-#resource "cloudflare_rate_limit" "limit" {
-#  zone_id             = lookup(data.cloudflare_zones.zone.zones[0], "id")
-#  threshold           = var.threshold
-#  period              = var.period
-#  disabled            = var.disabled
-#  description         = var.description
-#  bypass_url_patterns = var.bypass_url_patterns
-#
-#  match {
-#    request {
-#      url_pattern = var.request.url_pattern
-#      schemes     = var.request.schemes
-#      methods     = var.request.methods
-#    }
-#
-#    dynamic "response" {
-#      for_each = var.response == null ? [] : [1]
-#      content {
-#        statuses       = var.response.statuses
-#        origin_traffic = var.response.origin_traffic
-#        headers        = var.response.headers
-#      }
-#    }
-#  }
-#
-#  action {
-#    mode    = var.action.mode
-#    timeout = var.action.timeout
-#
-#    dynamic "response" {
-#      for_each = try(var.action.response, null) == null ? [] : [1]
-#      content {
-#        content_type = var.action.response.content_type
-#        body         = var.action.response.body
-#      }
-#    }
-#  }
-#
-#  dynamic "correlate" {
-#    for_each = var.correlate_by == null ? [] : [1]
-#    content {
-#      by = var.correlate_by
-#    }
-#  }
-#}
-#
-#
 resource "cloudflare_ruleset" "zone_level_ratelimit" {
   count = length(var.domains)
 
